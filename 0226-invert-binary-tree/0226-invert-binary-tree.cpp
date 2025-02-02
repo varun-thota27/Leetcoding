43c78fc652c28c0aa0ea2b45eb056ecb2c5101fd
Varun Thota <varunkittu289@gmail.com>
2025-02-02 21:34:53 +0530

    0226-invert-binary-tree.cpp

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -15,14 +15,12 @@ public:
         
         if(root==NULL)
         return NULL;
-
         TreeNode* temp=root->left;
         root->left=root->right;
         root->right=temp;
 
         invertTree(root->left);
         invertTree(root->right);
-
         return root;
     }
 };
