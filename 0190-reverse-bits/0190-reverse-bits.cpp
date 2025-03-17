de128553ed3c900ae0cdbfac07ce52dfe118566a
Varun Thota <varunkittu289@gmail.com>
2025-03-17 22:12:02 +0530

    Time: 5 ms (9.91%), Space: 7.9 MB (28.54%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            ans<<=1;
+            if(n&1)
+             ans=ans|1;
+
+            n>>=1;
+        }
+        return ans;
+    }
+};
