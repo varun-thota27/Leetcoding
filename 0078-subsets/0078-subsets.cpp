d49bacc6c818008a3bee78a7a86159462e495d94
Varun Thota <varunkittu289@gmail.com>
2025-04-25 14:20:32 +0530

    Time: 3 ms (33.13%), Space: 17 MB (7.19%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,29 +3,24 @@ public:
 
     void solve(vector<int> v,vector<vector<int>> &vec,int i,vector<int> ans)
     {
-        if(i>=v.size())
-        {
+        if(i==v.size()){
             vec.push_back(ans);
-            return;
+            return ;
         }
+
+
+        ans.push_back(v[i]);
+        solve(v,vec,i+1,ans);
+        ans.pop_back();
 
         solve(v,vec,i+1,ans);
-
-         if(i==0 || v[i]!=v[i-1] || !ans.empty() && ans.back()!= v [i])
-          {
-            ans.push_back(v[i]);
-            solve(v, vec, i + 1, ans);
-            ans.pop_back(); 
-        }
     }
     vector<vector<int>> subsets(vector<int>& v) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         vector<vector<int>> vec;
         vector<int> ans;
-        int i=0;
-        sort(v.begin(),v.end());
-        solve(v,vec,i,ans);
+        solve(v,vec,0,ans);
 
         return vec;
     }
