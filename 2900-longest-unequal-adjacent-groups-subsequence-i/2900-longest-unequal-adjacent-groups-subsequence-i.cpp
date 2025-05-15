3743f35d39850350fcdbad3ec003101e7642d770
Varun Thota <varunkittu289@gmail.com>
2025-05-15 16:53:33 +0530

    Time: 0 ms (100%), Space: 30.1 MB (25.85%) - LeetHub

---
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& w, vector<int>& g) {
+        vector<string> ans={w[0]};
+        for(int i=1;i<g.size();i++)
+        {
+            if(g[i]!=g[i-1])
+            ans.push_back(w[i]);
+        }
+        return ans;
+    }
+};
