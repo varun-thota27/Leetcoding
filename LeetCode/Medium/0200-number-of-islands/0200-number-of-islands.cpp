98bfa8d85a1d167b64df7c4a8732515f7ac2d76e
Varun Thota <varunkittu289@gmail.com>
2025-07-26 23:47:23 +0530

    Time: 29 ms (45.17%), Space: 17.5 MB (51.27%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    void bfs(int i,int j,vector<vector<char>>& v,vector<vector<int>>& vis)
+    void dfs(int i,int j,vector<vector<char>>& v,vector<vector<int>>& vis)
     {
         vis[i][j]=1;
-        queue<pair<int,int>> q;
-        q.push({i,j});
+        // queue<pair<int,int>> q;
+        // q.push({i,j});
         int n=v.size(),m=v[0].size();
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
-        while(!q.empty())
-        {
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
+        // while(!q.empty())
+        // {
+        //     int row=q.front().first;
+        //     int col=q.front().second;
+        //     q.pop();
             
             for(int k=0;k<4;k++){
-                int nrow= row+ drow[k];
-                int ncol= col + dcol[k];
+                int nrow= i+ drow[k];
+                int ncol= j + dcol[k];
 
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && v[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                    vis[nrow][ncol]=1;
-                    q.push({nrow,ncol});
+                    // vis[nrow][ncol]=1;
+                    // q.push({nrow,ncol});
+                    dfs(nrow,ncol,v,vis);
                 }
             }
-        }
+        // }
     }
     int numIslands(vector<vector<char>>& v) {
         int n=v.size(),m=v[0].size();
@@ -32,7 +33,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && v[i][j]=='1'){
-                    bfs(i,j,v,vis);
+                    dfs(i,j,v,vis);
                     c++;
                 }
             }
