766eabcb2090c46470f3d53314a815efba0c3164
Varun Thota <varunkittu289@gmail.com>
2025-07-29 22:58:54 +0530

    Time: 4 ms (97.72%), Space: 34.7 MB (79.37%) - LeetHub

--- LeetCode/Medium/0542-01-matrix/0542-01-matrix.cpp
+++ LeetCode/Medium/0542-01-matrix/0542-01-matrix.cpp
@@ -3,14 +3,14 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& v) {
         
         int n=v.size(),m=v[0].size();
-        vector<vector<int>> vis(n,vector<int> (m,0));
-        vector<vector<int>> ans(n,vector<int> (m));
-        queue<pair<pair<int,int>,int>> q;
+        // vector<vector<int>> vis(n,vector<int> (m,0));
+        vector<vector<int>> ans(n,vector<int> (m,-1));
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(v[i][j]==0){
-                    vis[i][j]=1;
-                    q.push({{i,j},0});
+                    ans[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
@@ -20,19 +20,18 @@ public:
 
         while(!q.empty())
         {
-            int row= q.front().first.first;
-            int col=q.front().first.second;
-            int steps= q.front().second;
+            int row= q.front().first;
+            int col=q.front().second;
             q.pop();
-            ans[row][col]=steps;
+            
 
             for(int i=0;i<4;i++){
                 int nrow= row + drow[i];
                 int ncol= col + dcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol]){
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},steps+1});
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==-1){
+                    ans[nrow][ncol]= ans[row][col] + 1;
+                    q.push({nrow,ncol});
                 }
             }
         }
