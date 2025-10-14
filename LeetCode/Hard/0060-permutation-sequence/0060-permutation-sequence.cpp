7527835f76612c8a07d0777ed90a22e6b1d07880
Varun Thota <varunkittu289@gmail.com>
2025-10-15 00:33:32 +0530

    Time: 57 ms (15.45%), Space: 8.5 MB (76.34%) - LeetHub

---
+++ LeetCode/Hard/0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string ans="";
+        for(int i=1;i<=n;i++){
+            ans+= to_string(i);
+        }
+
+        for(int i=0;i<k-1;i++){
+            next_permutation(ans.begin(),ans.end());
+        }
+        return ans;
+    }
+};
